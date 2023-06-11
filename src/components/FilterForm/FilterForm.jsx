67b24c4ef1902dfefb755fa2bb469c489e0241ef
Oleksiy Pokormyakho <116438996+OleksiyPok@users.noreply.github.com>
2023-06-11 11:35:08 +0200

    Section has been changed to function component

--- src/components/FilterForm/FilterForm.jsx
+++ src/components/FilterForm/FilterForm.jsx
@@ -1,11 +1,8 @@
-// import { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import { Container, Label, Input } from './FilterForm.styled';
 
-// class FilterForm extends Component {
 const FilterForm = ({ setFilter }) => {
-  // render() {
   return (
     <Container>
       <Label>Find contacts by name</Label>
@@ -16,14 +13,12 @@ const FilterForm = ({ setFilter }) => {
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
         autocomplete="off"
-        // onChange={this.props.setFilter}
         onChange={setFilter}
         required
       />
     </Container>
   );
 };
-// }
 
 export default FilterForm;
 
--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,25 +1,19 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import { Container, Title } from './Section.styled';
 
-class Section extends Component {
-  render() {
-    return (
-      <Container>
-        {this.props.title ? <Title>{this.props.title}</Title> : ''}
-        {this.props.children ? this.props.children : ''}
-      </Container>
-    );
-  }
-}
+const Section = ({ title, children }) => {
+  return (
+    <Container>
+      {title ? <Title>{title}</Title> : ''}
+      {children ? children : ''}
+    </Container>
+  );
+};
 
 export default Section;
 
 Section.propTypes = {
   title: PropTypes.string,
-  children: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.node),
-    PropTypes.node,
-  ]),
+  children: PropTypes.node,
 };
